07e5a364c28f461f7c898eb195f1d10054583a76
ralis <ralis.vojtech@email.cz>
2024-08-22 17:16:53 +0000

    Correct Board's init method

--- Othello/src/board/board.py
+++ Othello/src/board/board.py
@@ -8,6 +8,10 @@ class Board:
         self.height = height
         self.width = width
         self.board = [[Cell.EMPTY] * width for _ in range(height)]
+        self.board[height // 2 - 1][width // 2 - 1] = Cell.WHITE
+        self.board[height // 2][width // 2] = Cell.WHITE
+        self.board[height // 2 - 1][width // 2] = Cell.BLACK
+        self.board[height // 2][width // 2 - 1] = Cell.BLACK
 
     def __getitem__(self, index: int) -> list[Cell]:
         return self.board[index]
